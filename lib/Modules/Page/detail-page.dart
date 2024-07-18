7d2b2d488045fb10aeaa9c6de0e9f9032f05e4e0
svgsrc <20190808066@ogr.akdeniz.edu.tr>
2024-07-18 18:25:01 +0300

    edited QRScannerPage, added HomePage

---
+++ lib/Modules/Page/detail-page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
