e169c2fec699acf7e71540b70433f3cdf7a1edac
svgsrc <20190808066@ogr.akdeniz.edu.tr>
2024-07-24 19:12:16 +0300

    added daily report

--- lib/Widgets/apartment-card.dart
+++ lib/Widgets/apartment-card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:talya_flutter/Modules/Page/detail-page.dart';
-import 'package:talya_flutter/Modules/Models/Contact.dart';
 import 'package:talya_flutter/Modules/Models/Apartment.dart';
 
 
